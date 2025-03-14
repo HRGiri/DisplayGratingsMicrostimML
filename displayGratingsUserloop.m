c74f524f03f9047ab6db6e33db496fa814b9f6c7
Shashank Gouroju <shashank.gouroju@gmail.com>
2025-03-14 19:16:21 +0530

    added script to create stim table in a more generalized way - written by Pai

--- displayGratingsUserloop.m
+++ displayGratingsUserloop.m
@@ -12,36 +12,20 @@ persistent stimBorrow               % List of stimuli of the next block displaye
 
 % Createa table of all stimulus combinations and return timing file if it the very first call
 persistent stimTable
+persistent stimNum
 if isempty(stimTable)
     % Prerequisite variables (HARDCODED):
-    azi = 0; % Azimuths (deg), V1_dona = -1.75, V4_dona = -1.35
-    ele = 0; % Elevations (deg), V1_dona = -2.5, V4_dona = -0.6
-    radii = 2.^(6); % Aperture radii (deg)
-    sf = 0.5*(2.^(0)); % Spatial Frequencies (SFs) (cpd)
-    ori = [0, 45, 90]; % Orientations (deg)
-    con = 25*(2.^(2)); % Contrasts (%)
-    RF = ["IN"]; % Receptive Field (RF) conditions, IN/OUT
+    params.RF = ["IN"]; % Receptive Field (RF) conditions, IN/OUT
+    params.azi = 0; % Azimuths (deg), V1_dona = -1.75, V4_dona = -1.35
+    params.ele = 0; % Elevations (deg), V1_dona = -2.5, V4_dona = -0.6
+    params.radii = 2.^(6); % Aperture radii (deg)
+    params.sf = 0.5*(2.^(0)); % Spatial Frequencies (SFs) (cpd)
+    params.ori = [0, 45, 90]; % Orientations (deg)
+    params.con = 25*(2.^(2)); % Contrasts (%)
 
     % Creating the stimulus table:
-    stimTable = table('Size', [length(RF)*length(azi)*length(ele)*length(sf)*length(ori)*length(con)*length(radii), 7], 'VariableNames', ...
-        ["RF", "azi", "ele", "radii", "sf", "ori","con"], 'VariableTypes', ["string", "double", "double", "double", "double", "double", "double"]); % MATLAB is too retarded to build DataFrames like Pandas:
-    row = 1;
-    for i = 1:length(RF)
-        for j = 1:length(azi)
-            for k = 1:length(ele)
-                for m = 1:length(radii)
-                    for n = 1:length(sf)
-                        for o = 1:length(ori)
-                            for p = 1:length(con)
-                                stimTable{row, :} = [RF(i), azi(j), ele(k), radii(m), sf(n), ori(o), con(p)];
-                                row = row + 1;
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
+    stimTable = create_stimtable(params=params);
+    stimNum = size(stimTable, 1);
     return
 end
 
@@ -54,12 +38,12 @@ if isempty(TrialRecord.TrialErrors)                                         % If
     condition = 1;                                                          % set the condition # to 1
 elseif ~isempty(TrialRecord.TrialErrors) && 0==TrialRecord.TrialErrors(end) % If the last trial is a success
     stimList = setdiff(stimList, stimPrev);                                 % remove previous trial stimuli from the list of stimuli
-    condition = mod(condition+stim_per_trial-1, size(stimTable, 1))+1;      % increment the condition # by stim_per_trial
+    condition = mod(condition+stim_per_trial-1, stimNum)+1;                 % increment the condition # by stim_per_trial
 end
 
 % Initialize the conditions for a new block
 if isempty(stimList)                                            % If there are no stimuli left in the block
-    stimList = setdiff(1:size(stimTable, 1), stimBorrow);       %
+    stimList = setdiff(1:stimNum, stimBorrow);       %
     stimBorrow = [];
     block=block+1;
 end
@@ -69,7 +53,7 @@ if length(stimList)>=stim_per_trial                                         % If
     stimPrev = stimCurrent;
 else
     stimPrev = stimList;
-    stimBorrow = datasample(imageNum, stim_per_trial-length(stimList), 'Replace', false);
+    stimBorrow = datasample(stimNum, stim_per_trial-length(stimList), 'Replace', false);
     stimCurrent = [stimList stimBorrow];
     stimCurrent = stimCurrent(randperm(stim_per_trial));
 end
